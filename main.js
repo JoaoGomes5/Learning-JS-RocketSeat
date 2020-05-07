03fbe1926c6e48a2defdee433cbe50e4e0ec5b6a
Joao Gomes <joaopfg.2002@gmail.com>
2020-05-07 19:10:11 +0100

    valores padrao para func

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-const arr = [1,2,3,4,5,6];
-
-const newArr = arr.map(item => item * 2 );
-
-console.log(newArr);
-
-const teste = () => ({nome:"joao"});
-
-console.log(teste());
+const sum = (a = 3,b = 6) => a + b;  
 
 
- 
+console.log(sum(1));
+console.log(sum());
