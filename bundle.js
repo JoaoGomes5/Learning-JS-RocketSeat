95a2a168e818bda0b6847e79029ac97043ffa4ed
Joao Gomes <joaopfg.2002@gmail.com>
2020-05-07 18:48:40 +0100

    Consts

--- bundle.js
+++ bundle.js
@@ -1,24 +1,12 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function teste(x) {
+  var y = 2;
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Mat = /*#__PURE__*/function () {
-  function Mat() {
-    _classCallCheck(this, Mat);
+  if (x > 5) {
+    var _y = 4;
+    console.log(x, _y);
   }
+}
 
-  _createClass(Mat, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Mat;
-}();
-
-console.log(Mat.soma(1, 2));
+teste(10);
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-class Mat{
-    static soma(a,b){
-        return a + b ;
+function teste(x){
+    let y = 2;
+
+    if(x>5){ 
+        let y = 4;
+        console.log(x,y);
+
     }
 }
 
-
-console.log(Mat.soma(1,2));
+teste(10);
