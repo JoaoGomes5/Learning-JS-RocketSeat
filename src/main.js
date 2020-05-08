db9f8ed3b9cfa9adc251706354e09cec3328609f
Joao Gomes <joaopfg.2002@gmail.com>
2020-05-08 15:50:29 +0100

    render perfect

--- src/main.js
+++ src/main.js
@@ -1,20 +1,104 @@
-import axios from 'axios';
+import api from './api';
+class App{
 
-class Api{
+    constructor(){
 
-    static async getUserInfo(username){
+        this.repositores = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandles();
+    }
+
+    registerHandles(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    setLoading(Loading = true){
+
+        if (Loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Loading...'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        }else{
+
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event){
+
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0)
+            return;
+
+        this.setLoading();
+
         try{
-            
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-              console.log(response);
 
-        }catch(arr){
-            console.warn('Erro na API');
+            const response = await api.get(`/repos/${repoInput}`);
+    
+            const {name, description, html_url, owner: {avatar_url}} = response.data;
+    
+            this.repositores.push({
+    
+                name,
+                description,
+                avatar_url,
+                html_url
+    
+            });
+    
+            this.inputEl.value = '';
+    
+            this.render();
+
+        }catch(err){
+            alert('O repositorio não existe');
         }
 
-      
-
+        this.setLoading(false);
+        
     }
+    
+    render(){
+
+        this.listEl.innerHTML = '';
+
+        this.repositores.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src' , repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target' , '_blank');
+            linkEl.setAttribute('href' , repo.html_url);
+            linkEl.appendChild(document.createTextNode('Ir para o repositório'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+    }
+
+
 }
 
-Api.getUserInfo('gustavoguanabara');
+new App();
